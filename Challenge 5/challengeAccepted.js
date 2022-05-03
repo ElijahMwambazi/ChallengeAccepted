a4422afb83a99d00811e61aaf66e1fe01f447eb9
Elijah Mwambazi <elijahmwambaxi@gmail.com>
2022-05-03 23:50:26 +0200

    Solved 'the hurdle race challenge'

--- Challenge 5/challengeAccepted.js
+++ Challenge 5/challengeAccepted.js
@@ -1,63 +1,83 @@
-// TODO: Grading Students
-function gradingStudents(grades) {
-    grades.forEach(function (grade, index) {
-        if (grade >= 38) {
-            let nextMultiple = Math.ceil(grade / 5) * 5;
-            let difference = nextMultiple - grade;
+/*********************  
+HACKER RANK CHALLENGES 
+**********************/
 
-            if (difference < 3) {
-                grades[index] = nextMultiple;
-            } else {
-                grades[index] = grade;
-            }
-        } else {
-            grades[index] = grade;
-        }
-    });
-    console.log(grades);
-}
+// // TODO: Grading Students
+// function gradingStudents(grades) {
+//     grades.forEach(function (grade, index) {
+//         if (grade >= 38) {
+//             let nextMultiple = Math.ceil(grade / 5) * 5;
+//             let difference = nextMultiple - grade;
 
-gradingStudents([73, 67, 38, 33, 40]);
+//             if (difference < 3) {
+//                 grades[index] = nextMultiple;
+//             } else {
+//                 grades[index] = grade;
+//             }
+//         } else {
+//             grades[index] = grade;
+//         }
+//     });
+//     return grades;
+// }
 
-// TODO: Counting Number Of Pairs
-function countNumberOfPairs(arr) {
-    let pairsCount = 0;
-    let counterObj = {};
+// console.log(gradingStudents([73, 67, 38, 33, 40]));
 
-    for (let num of arr) {
-        if (!counterObj[num]) {
-            counterObj[num] = 1;
-        } else {
-            counterObj[num] = counterObj[num] + 1;
+// // TODO: Counting Number Of Pairs
+// function countNumberOfPairs(arr) {
+//     let pairsCount = 0;
+//     let counterObj = {};
 
-            if (counterObj[num] === 2) {
-                pairsCount++;
-                counterObj[num] = 0;
-            }
+//     for (let num of arr) {
+//         if (!counterObj[num]) {
+//             counterObj[num] = 1;
+//         } else {
+//             counterObj[num] = counterObj[num] + 1;
+
+//             if (counterObj[num] === 2) {
+//                 pairsCount++;
+//                 counterObj[num] = 0;
+//             }
+//         }
+//     }
+//     return pairsCount
+// }
+
+// console.log(countNumberOfPairs([1, 2, 1, 2, 1, 3, 2, 3]));
+
+// // TODO: Counting Valleys
+// function countingValleys(steps, path) {
+//     let numValley = 0;
+//     let level = 0;
+
+//     for (let item = 0; item < steps; item++) {
+//         if (path[item] === "U") {
+//             level++;
+//             if (level === 0) {
+//                 numValley++;
+//             }
+//         } else {
+//             level--;
+//         }
+//     }
+
+//     return numValley;
+// }
+
+// console.log(countingValleys(12, "DDUUDDUDUUUDUU"));
+
+// TODO: The Hurdle Race
+function hurdleRace(k, height) {
+    let max = 0;
+
+    for (let i = 0; i < height.length; i++) {
+        if (height[i] > max) {
+            max = height[i];
         }
     }
-    console.log(pairsCount);
+
+    let potionNum = max - k;
+    return potionNum < 0 ? 0 : potionNum;
 }
 
-countNumberOfPairs([1, 2, 1, 2, 1, 3, 2, 3]);
-
-// TODO: Counting Valleys
-function countingValleys(steps, path) {
-    let numValley = 0;
-    let level = 0;
-
-    for (let item = 0; item < steps; item++) {
-        if (path[item] === "U") {
-            level++;
-            if (level === 0) {
-                numValley++;
-            }
-        } else {
-            level--;
-        }
-    }
-
-    return numValley;
-}
-
-console.log(countingValleys(12, "DDUUDDUDUUUDUU"));
+console.log(hurdleRace(10, [4, 5, 6, 20, 7, 8, 9, 10]));
